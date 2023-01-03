ce64a698ecfe5d463cd0b1dde2bc8e4fd4e9f219
adachkamil98 <adachkamil98@gmail.com>
2023-01-04 00:50:51 +0100

    separete functions were created

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,42 @@
-{
-  console.log("Hello World!!!");
+const welcome = () => {
+  console.log("Hello World!")
+}
 
-const formElement = document.querySelector(".js-form");
-const resultElement = document.querySelector(".js-result");
-const inputElement = document.querySelector(".js-inputValue");
-const currencyUnit = document.querySelector(".js-currency");
+const init = () => {
+  const formElement = document.querySelector(".js-form");
 
-const exchangeRates = {
-  USD: 4.4153,
-  EUR: 4.6886,
-  GBP: 5.3926,
-  JPY: 3.2436,
-  AUD: 2.9666,
-  CAD: 3.2354,
-  CHF: 4.7453,
-  UAH: 0.1258,
-  SEK: 0.4266,
-  NZD: 2.8232,
-};
+  formElement.addEventListener("submit", onFormSubmit);
+}
 
-formElement.addEventListener("submit", (event) => {
+const onFormSubmit = (event) => {
   event.preventDefault();
+
+  const exchangeRates = {
+    USD: 4.4153,
+    EUR: 4.6886,
+    GBP: 5.3926,
+    JPY: 3.2436,
+    AUD: 2.9666,
+    CAD: 3.2354,
+    CHF: 4.7453,
+    UAH: 0.1258,
+    SEK: 0.4266,
+    NZD: 2.8232,
+  };
+
+  const inputElement = document.querySelector(".js-inputValue");
+  const currencyUnit = document.querySelector(".js-currency");
+  const resultElement = document.querySelector(".js-result");
 
   const amount = +inputElement.value;
   const currency = currencyUnit.value;
   const result = amount / exchangeRates[currency];
-
   resultElement.innerText = `${result.toFixed(2)} ${currency}`;
-});
+
+  onFormSubmit();
 }
+
+
+welcome();
+
+init();
