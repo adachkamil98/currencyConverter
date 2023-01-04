7ba0e91a0ce21762e271e5297b5fc8af2520f344
adachkamil98 <adachkamil98@gmail.com>
2023-01-04 20:30:44 +0100

    gif added to README

--- JS/script.js
+++ JS/script.js
@@ -1,42 +1,43 @@
-const welcome = () => {
-  console.log("Hello World!")
+{
+  const welcome = () => {
+    console.log("Hello World!")
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const exchangeRates = {
+      USD: 4.4153,
+      EUR: 4.6886,
+      GBP: 5.3926,
+      JPY: 3.2436,
+      AUD: 2.9666,
+      CAD: 3.2354,
+      CHF: 4.7453,
+      UAH: 0.1258,
+      SEK: 0.4266,
+      NZD: 2.8232,
+    };
+
+    const inputElement = document.querySelector(".js-inputValue");
+    const currencyUnit = document.querySelector(".js-currency");
+    const resultElement = document.querySelector(".js-result");
+
+    const amount = +inputElement.value;
+    const currency = currencyUnit.value;
+    const result = amount / exchangeRates[currency];
+    resultElement.innerText = `${result.toFixed(2)} ${currency}`;
+
+    onFormSubmit();
+  }
+
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit);
+  }
+
+  welcome();
+
+  init();
 }
-
-const init = () => {
-  const formElement = document.querySelector(".js-form");
-
-  formElement.addEventListener("submit", onFormSubmit);
-}
-
-const onFormSubmit = (event) => {
-  event.preventDefault();
-
-  const exchangeRates = {
-    USD: 4.4153,
-    EUR: 4.6886,
-    GBP: 5.3926,
-    JPY: 3.2436,
-    AUD: 2.9666,
-    CAD: 3.2354,
-    CHF: 4.7453,
-    UAH: 0.1258,
-    SEK: 0.4266,
-    NZD: 2.8232,
-  };
-
-  const inputElement = document.querySelector(".js-inputValue");
-  const currencyUnit = document.querySelector(".js-currency");
-  const resultElement = document.querySelector(".js-result");
-
-  const amount = +inputElement.value;
-  const currency = currencyUnit.value;
-  const result = amount / exchangeRates[currency];
-  resultElement.innerText = `${result.toFixed(2)} ${currency}`;
-
-  onFormSubmit();
-}
-
-
-welcome();
-
-init();
